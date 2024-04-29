acb9907b529340ed9bbf3154d4b60efd70c30098
Faseeh <bscs.f20.m.33@gmail.com>
2024-04-29 08:30:33 +0500

    index file edited

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,11 +1,11 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter} from "react-router-dom";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 import HomeLayout from "../layouts/HomeLayout";
 import ProfileLayout from "../layouts/ProfileLayout";
 import ConnectionLayout from "../layouts/ConnectionLayout";
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
